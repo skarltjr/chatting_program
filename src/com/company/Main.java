501e085d3b123fd95a201e66766bd2c80da8d552
skarltjr <kisa0828@naver.com>
2021-12-17 02:45:06 +0900

    feat(main) : add main of program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,34 @@
 package com.company;
 
-public class Main {
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    private static final int PORT = 8080;
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world");
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        try {
+            // bind
+            serverSocket = new ServerSocket(PORT);
+            // accept
+            socket = serverSocket.accept();
+            // accept 이후 메세진 송 수신을 각 스레드를 새로 할당하여 담당
+            SenderThread senderThread = new SenderThread(socket);
+            ReceiveThread receiveThread = new ReceiveThread(socket);
+            senderThread.start();
+            receiveThread.start();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } finally {
+            // 모두 종료 후 serversocket close
+            try {
+                serverSocket.close();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 }
